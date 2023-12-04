7e00dfde7f93d2107584918aa893db3a9530c803
Sean <sean.leu@gmail.com>
2023-12-03 20:56:26 -0500

    Update

--- AoC-Day3-23.py
+++ AoC-Day3-23.py
@@ -1,10 +1,17 @@
-sum = 0
+sum1 = 0
+sum2 = 0
 the_map = []
 row_length = 0
 col_length = 0
 
 def is_symbol(value):
-    if not value.isdigit() and not value == ".":
+    if not value.isdigit() and value != ".":
+        return True
+    else:
+        return False
+
+def is_gear(value):
+    if value == "*":
         return True
     else:
         return False
@@ -13,54 +20,88 @@ def check_right(x,y):
     number = ""
     z=1
     while x+z < row_length:
-        position = the_map[y][x+z]
-        if position.isdigit():
-            number.join(str(position))
-            position = "."
+        if the_map[y][x+z].isdigit():
+            number += str(the_map[y][x+z])
+            the_map[y][x+z] = "."
             z+=1
         else:
             z+=row_length
     if number != "":
-        sum += int(number)
+        return int(number)
+    else:
+        return 0
 
 def check_left(x,y):
     number = ""
     z=1
     while x-z >= 0:
-        position = the_map[y][x-z]
-        if position.isdigit():
-            number.join(str(position))
-            position = "."
+        if the_map[y][x-z].isdigit():
+            number += str(the_map[y][x-z])
+            the_map[y][x-z] = "."
             z+=1
         else:
             z+=row_length
     if number != "":
-        sum += int(number)
+        number_reverse = number[::-1]
+        return int(number_reverse)
+    else:
+        return 0
+
 def check_up(x,y):
     number = ""
-    z=1
-    while y-z >= 0:
-        position = the_map[y-z][x]
-        if position.isdigit():
-            number.join(str(position))
-            position = "."
-            z+=1
-        else:
-            z+=col_length
+    if y <= 0: #at top, no higher
+        return 0
+    else:
+        up1 = y-1
+    if the_map[up1][x].isdigit():
+        if x-1 >= 0: #<---
+            if the_map[up1][x-1].isdigit():
+                if x-2 >= 0:
+                    if the_map[up1][x-2].isdigit():
+                        number += str(the_map[up1][x-2])
+                        the_map[up1][x-2] = "."
+                number += str(the_map[up1][x-1])
+                the_map[up1][x-1] = "."
+        number += str(the_map[up1][x]) #center
+        the_map[up1][x] = "."
+        if x+1 < col_length: #--->
+            if the_map[up1][x+1].isdigit():
+                number +=str (the_map[up1][x+1])
+                the_map[up1][x+1] = "."
+                if x+2 < col_length:
+                    if the_map[up1][x+2].isdigit():
+                        number += str(the_map[up1][x+2])
+                        the_map[up1][x+2] = "."
     if number != "":
-        sum += int(number)
+        return int(number)
+    else:
+        return 0
+
 def check_down(x,y):
     number = ""
-    z=1
-    while y+z < col_length:
-        position = the_map[y+z][x]
-        if position.isdigit(): #check left and right to build number properlys
-            number+=str(position)
-            
-            position = "."
-            z+=1
-        else:
-            z+=col_length
+    if y >= col_length-1: #at bottom, no down
+        return 0
+    else:
+        down1 = y+1
+    if the_map[down1][x].isdigit():
+        if x-1 >= 0: #<---
+            if the_map[down1][x-1].isdigit():
+                if x-2 >= 0:
+                    if the_map[down1][x-2].isdigit():
+                        number += str(the_map[down1][x-2])
+                        the_map[down1][x-2] = "."
+                number += str(the_map[down1][x-1])
+                the_map[down1][x-1] = "."
+        number += str(the_map[down1][x]) #center
+        the_map[down1][x] = "."
+        if x+1 < col_length: #--->
+            if the_map[down1][x+1].isdigit():
+                number += str(the_map[down1][x+1])
+                the_map[down1][x+1] = "."
+                if x+2 < col_length:
+                    if the_map[down1][x+2].isdigit():
+                        number += str(the_map[down1][x+2])
+                        the_map[down1][x+2] = "."
     if number != "":
         return int(number)
     else:
@@ -68,34 +109,69 @@ def check_down(x,y):
 
 def check_adjacent(x,y):
     total = 0
-    #check_up(x,y)
+    total += check_up(x,y)
     total += check_down(x,y)
-    #check_left(x,y)
-    #check_right(x,y)
+    total += check_left(x,y)
+    total += check_right(x,y)
+    total += check_right(x,y+1) #lower right corner
+    total += check_right(x,y-1) #upper right corner
+    total += check_left(x,y+1) #lower left corner  
+    total += check_left(x,y-1) #upper left corner
     return total
 
+def gear_ratio(x,y):
+    values = []
+    up = check_up(x,y)
+    if up > 0: values.append(up)
 
+    down = check_down(x,y)
+    if down > 0: values.append(down)
 
+    left = check_left(x,y)
+    if left > 0: values.append(left)
+    
+    if len(values) < 3:
+        right = check_right(x,y)
+        if right > 0: values.append(right)
 
-with open('inputs/sample.txt') as i: input = i.read().splitlines()
+    if len(values) < 3: 
+        lright = check_right(x,y+1)
+        if lright > 0: values.append(lright) #lower right corner
+
+    if len(values) < 3: 
+        uright = check_right(x,y-1)
+        if uright > 0: values.append(uright) #upper right corner
+        
+    if len(values) < 3: 
+        lleft = check_left(x,y+1)
+        if lleft > 0: values.append(lleft) #lower left corner
+        
+    if len(values) < 3: 
+        uleft = check_left(x,y-1)
+        if uleft > 0: values.append(uleft) #upper left corner
+    
+    if len(values) == 2:
+        return values[0] * values[1]
+    else:
+        return 0
+
+with open('inputs/input03.txt') as i: input = i.read().splitlines()
 
 for line in input: #builds map
     row = []
     for value in line:
         row.append(value)
     the_map.append(row)
-    #print(row)
 
 row_length = len(the_map[0])
 col_length = len(the_map)
 
 for y,row in enumerate(the_map):
-    for x,value in enumerate(row):
-        if is_symbol(value):
-            print("value:", value)
-            sum += check_adjacent(x,y)
-print(sum)
+    for x,value in enumerate(row): #comment out due to map clears on each use; can only run one
+        """if is_symbol(value):
+            sum1 += check_adjacent(x,y)"""
+        if is_gear(value):
+            sum2 += gear_ratio(x,y)
 
-#number isn't being build
-#number could be in left or right or center direction
-#fix then account for diagnals
+print("Part 1:", sum1)
+print("Part 2: ", sum2)
